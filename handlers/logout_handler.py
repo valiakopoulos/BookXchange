02f535b75a29d061955ff864914ee59fb955407b
slhawkins <steve@slhawkins.com>
2017-04-09 17:02:02 -0500

    Fixed Logout

--- handlers/logout_handler.py
+++ handlers/logout_handler.py
@@ -3,11 +3,11 @@ import logging, os
 
 class LogoutHandler(BaseHandler):
     def get(self):
-        if 'BOOKXCHANGE_PROD' in os.environ:
-            return self.redirect('https://bookxchange-cs595.appspot.com/saml/?slo')
         try:
             del self.session['email']
         except KeyError:
             # This will happen if the user is already logged out. Ignore it and move on with life
             pass
+        if 'BOOKXCHANGE_PROD' in os.environ:
+            return self.redirect('https://bookxchange-cs595.appspot.com/saml/?slo')
         return self.redirect('/')
